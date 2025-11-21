21718954ff10e5707e5a87cc848989e2d04aaaf1
Daniel Schmolke <schmolke@gmail.com>
2025-11-21 20:28:14 +0100

    wip

--- .config/nvim/lua/plugins/tasks.lua
+++ .config/nvim/lua/plugins/tasks.lua
@@ -6,6 +6,54 @@ return {
   -- lazy = false,
 
   config = function()
+    local function multiline(lead, args)
+      local parts = vim.split(args, "%s+")
+      local _, machine, type, build_arg_1 = unpack(parts)
+      local arg_index = #parts - 1 -- # is length of the thing
+      local machines = { "TD9", "TD10" }
+      local types = {
+        TD10 = { "standard", "LowDraft", "SlowSpeed" },
+        TD9 = { "double", "single" },
+      }
+      local build_args = {
+        "--fastbuild",
+        "--copysim",
+      }
+
+      if arg_index == 1 then
+        return vim.tbl_filter(function(item)
+          return item:find("^" .. machine)
+        end, machines)
+      elseif arg_index == 2 then
+        return vim.tbl_filter(function(item)
+          return item:find("^" .. type)
+        end, types[machine])
+      elseif arg_index == 3 then
+        return vim.tbl_filter(function(item)
+          return item:find("^" .. lead)
+        end, build_args)
+      elseif arg_index == 4 then
+        local item = {}
+        if build_arg_1 == build_args[1] then
+          item[1] = build_args[2]
+        else
+          item[1] = build_args[1]
+        end
+        return item
+      end
+    end
+
+    -- multi complete build command test
+    vim.api.nvim_create_user_command("Build", function(opts)
+      local machine, type, build_arg_1, build_arg_2 = unpack(vim.split(opts.args, "%s"))
+      -- if not build_arg_1 then build_arg_1  = "" end
+      -- if not build_arg_2 then build_arg_2 = "" end
+      print("Args: " .. opts.args)
+      print(
+        "machine: " .. machine .. " type: " .. type .. " build_arg_1: " .. build_arg_1 .. "build_arg_2: " .. build_arg_2
+      )
+    end, { nargs = "*", complete = multiline })
+
     -- KSZ7
     vim.api.nvim_create_user_command("BuildTD10standardKSZ7", function()
       vim.cmd("split | terminal")
@@ -22,7 +70,7 @@ return {
     end, {})
 
     -- KSZ6
-    vim.api.nvim_create_user_command("BuildTD10standardKSZ6", function()
+    vim.api.nvim_create_user_command("BuildTD10standardKSZ6", function(opts)
       vim.cmd("split | terminal")
       local buf = vim.api.nvim_create_buf(false, true)
       vim.api.nvim_set_current_buf(buf)
